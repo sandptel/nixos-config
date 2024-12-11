26a90aea948a059abe337ed247611b92eacdb85a
sandptel <sandeeppatel626735@gmail.com>
2024-12-11 16:12:25 +0530

    minor fixes

--- hosts/default/users.nix
+++ hosts/default/users.nix
@@ -75,7 +75,7 @@ github-desktop
   programs.fish.enable = true;
   programs.fish.interactiveShellInit = ''
     #fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
-        #pokemon colorscripts like. Make sure to install krabby package
+        #pokemon colorscripts 
         krabby random --no-mega --no-gmax --no-regional --no-title -s;
         wal --preview | sed '/Current colorscheme:/d'
   '';
