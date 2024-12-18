3eeaf3eeaf85d61491cf7e1ec448af4a9db1293c
sandptel <sandeeppatel626735@gmail.com>
2024-12-18 21:51:41 +0530

    wallpaper: everything stablised :)/ Gpt is shit afterall

--- config/hypr/UserScripts/WallpaperRandom.sh
+++ config/hypr/UserScripts/WallpaperRandom.sh
@@ -2,24 +2,33 @@
 # /* ---- 👒 https://github.com/sandptel/nixos-config ---- */  ##
 # Script for Random Wallpaper ( CTRL ALT W)
 
+allwallDIR="$HOME/Pictures/wallpapers"
 wallDIR="$HOME/Pictures/wallpapers/Best"
 scriptsDir="$HOME/.config/hypr/scripts"
 
 focused_monitor=$(hyprctl monitors | awk '/^Monitor/{name=$2} /focused: yes/{print name}')
 
-# Find all images, excluding the previously used wallpaper
-if [[ -f "$wallDIR/current_wallpaper.txt" ]]; then
-    last_wallpaper=$(cat "$wallDIR/current_wallpaper.txt")
-    PICS=($(find "$wallDIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \) ! -name "$last_wallpaper"))
-    # Remove all existing .txt files in the wallpaper directory
-    find "$wallDIR" -type f -name "*.txt" -exec rm {} \;
-else
-    PICS=($(find "$wallDIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \)))
-fi
+# THis method is shit :/ as much as gpt itself is :)
+# {# Find all images, excluding the previously used wallpaper
+# if [[ -f "$wallDIR/current_wallpaper.txt" ]]; then
+#     last_wallpaper=$(cat "$wallDIR/current_wallpaper.txt")
+#     PICS=($(find "$wallDIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \) ! -name "$last_wallpaper"))
+#     # Remove all existing .txt files in the wallpaper directory
+#     find "$wallDIR" -type f -name "*.txt" -exec rm {} \;
+# else
+#     PICS=($(find "$wallDIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \)))
+# fi }}
+
+
+last_wallpaper=$(cat "$wallDIR/current_wallpaper.txt")
+find "$wallDIR" -type f -name "*.txt" -exec rm {} \;
+mapfile -d '' PICS < <(find "${wallDIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -print0 ! -name "$last_wallpaper")
+
 
 # Select a random wallpaper from the filtered list
 if [[ ${#PICS[@]} -eq 0 ]]; then
     echo "No wallpapers left to choose from!"
+    mapfile -d '' PICS < <(find "${allwallDIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -print0 ! -name "$last_wallpaper")
     exit 1
 fi
 RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
@@ -30,7 +39,7 @@ basename "$(basename "$RANDOMPICS")" > "$wallDIR/current_wallpaper.txt"
 # Transition config
 FPS=120
 TYPE="any"
-DURATION=1.5
+DURATION=1
 BEZIER=".43,1.19,1,.4"
 SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION --transition-bezier $BEZIER"
 
